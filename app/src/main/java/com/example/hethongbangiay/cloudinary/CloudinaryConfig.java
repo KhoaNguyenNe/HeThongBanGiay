f094aa813c631adde124d28bd9464bb0531510ba
PNamGP1120 <phuongnam.it0212@gmail.com>
2026-04-13 18:29:06 +0700

    cloudinary to shoes_app folder
    # Conflicts:
    #	app/src/main/java/com/example/hethongbangiay/MyApplication.java

--- app/src/main/java/com/example/hethongbangiay/cloudinary/CloudinaryConfig.java
+++ app/src/main/java/com/example/hethongbangiay/cloudinary/CloudinaryConfig.java
@@ -8,4 +8,5 @@ public final class CloudinaryConfig {
 
     public static final String CLOUD_NAME = "dqpu49bbo";
     public static final String UPLOAD_PRESET = "mobile_shoes_store_unsigned";
+    public static final String FOLDER = "shoes_app";
 }
