d18ef1676be9aa37be0433ca65a7e25f31e99700
VoVanMai <maivo0902@gmail.com>
2026-04-15 01:06:05 +0700

    update á»orderdetail

--- app/src/main/java/com/example/hethongbangiay/activities/OrdersFragment.java
+++ app/src/main/java/com/example/hethongbangiay/activities/OrdersFragment.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +20,7 @@ import com.example.hethongbangiay.R;
 import com.example.hethongbangiay.adapters.OrderAdapter;
 import com.example.hethongbangiay.models.DonHang;
 import com.example.hethongbangiay.repositories.DonHangRepository;
+import com.example.hethongbangiay.viewmodels.OrderViewModel;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -29,10 +32,7 @@ public class OrdersFragment extends Fragment {
     private RecyclerView rcvOrder;
     private OrderAdapter adapter;
     private List<DonHang> list;
-
     private DonHangRepository repository;
-
-    public OrdersFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -43,27 +43,38 @@ public class OrdersFragment extends Fragment {
         rcvOrder = view.findViewById(R.id.rcvOrder);
 
         list = new ArrayList<>();
-        adapter = new OrderAdapter(list);
+
+        adapter = new OrderAdapter(list, donHang -> {
+            Log.d("TEST", "Click detail order");
+            OrderDetailFragment fragment = new OrderDetailFragment();
+
+            Bundle bundle = new Bundle();
+            bundle.putSerializable("donHang", donHang);
+            fragment.setArguments(bundle);
+            requireActivity().findViewById(R.id.scrollContent).setVisibility(View.GONE);
+
+            getParentFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container,fragment)
+                    .addToBackStack(null)
+                    .commit();
+        });
 
         rcvOrder.setLayoutManager(new LinearLayoutManager(getContext()));
         rcvOrder.setAdapter(adapter);
 
         repository = new DonHangRepository();
-
         loadData();
 
         return view;
     }
 
     private void loadData() {
-
         repository.getAllDonHang(new DonHangRepository.OnDataLoaded() {
             @Override
             public void onSuccess(List<DonHang> data) {
-
                 list.clear();
                 list.addAll(data);
-
                 adapter.notifyDataSetChanged();
             }
 
--- app/src/main/java/com/example/hethongbangiay/viewmodels/OrderViewModel.java
+++ app/src/main/java/com/example/hethongbangiay/viewmodels/OrderViewModel.java
@@ -3,8 +3,34 @@ package com.example.hethongbangiay.viewmodels;
 import android.view.View;
 
 import androidx.databinding.BaseObservable;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class OrderViewModel extends ViewModel {
+import com.example.hethongbangiay.models.DonHang;
+import com.example.hethongbangiay.repositories.DonHangRepository;
 
+import java.util.List;
+
+public class OrderViewModel extends ViewModel {
+    private MutableLiveData<List<DonHang>> listDonHang = new MutableLiveData<>();
+    private DonHangRepository repository = new DonHangRepository();
+
+    public LiveData<List<DonHang>> getDonHang() {
+        return listDonHang;
+    }
+
+    public void loadDonHang() {
+        repository.getAllDonHang(new DonHangRepository.OnDataLoaded() {
+            @Override
+            public void onSuccess(List<DonHang> data) {
+                listDonHang.setValue(data);
+            }
+
+            @Override
+            public void onError(Exception e) {
+
+            }
+        });
+    }
 }
